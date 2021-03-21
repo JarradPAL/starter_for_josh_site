54472cb36fd1dda4354d5b42da4180d32a21d8a1
JarradPAL <jarradjpalmer@gmail.com>
2021-03-21 19:55:37 +0800

    Create footer.php

---
+++ footer.php
@@ -0,0 +1,24 @@
+<footer class="container-fluid">
+        <!-- dic for the main image to stretch to the edges of the page-->
+        <div class="container">
+
+            <div class="row">
+                <div class="col-md-6 text-center footer-content">
+                    <p> <?php bloginfo('name'); ?></p>
+
+                </div>
+                <div class="col-md-6 text-center footer-content">
+                    <p>Terms and Conditions</p>
+                </div>
+
+            </div>
+        </div>
+    </footer>
+
+
+
+   
+</body>
+
+</html>
+<?php wp_footer(); ?>
